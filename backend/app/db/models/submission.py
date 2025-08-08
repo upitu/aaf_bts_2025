f21875ea897d0bd917c6f46424e30fbd1c7df94f
Elias <79130002+upitu@users.noreply.github.com>
2025-08-08 15:44:56 +0300

    Fixing FastAPI Base DB Import in backend/app/db/models/submission.py V2

--- backend/app/db/models/submission.py
+++ backend/app/db/models/submission.py
@@ -10,10 +10,13 @@ class Submission(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     
-    first_name = Column(EncryptedType(String, encryption_key), nullable=False)
-    last_name = Column(EncryptedType(String, encryption_key), nullable=False)
-    email = Column(EncryptedType(String, encryption_key), unique=True, index=True, nullable=False)
-    
+    name = Column(EncryptedType(String, encryption_key), nullable=False)
+    email = Column(EncryptedType(String, encryption_key), index=True, nullable=False)
+    mobile = Column(EncryptedType(String, encryption_key), index=True, nullable=False)
+    emirates_id = Column(EncryptedType(String, encryption_key), nullable=False)
     emirate = Column(String, index=True, nullable=False)
+    
+    receipt_url = Column(String, nullable=False)
+    receipt_hash = Column(String, index=True, nullable=True) 
     
     submitted_at = Column(DateTime(timezone=True), server_default=func.now())
